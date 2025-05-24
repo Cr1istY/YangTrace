c0710fc65649cc93598b765001565cbbd90a7c1c
Crist Yang <cr1st4ever@outlook.com>
2025-05-24 10:05:49 +0800

    Ver1.1 增加了检错弹窗； 1. 输入网址过短 2. 输入网址找不到ip地址

--- core/TraceGui.py
+++ core/TraceGui.py
@@ -1,6 +1,6 @@
 import sys
 import socket
-from PySide6.QtWidgets import QWidget, QApplication, QTableWidgetItem, QHeaderView
+from PySide6.QtWidgets import QWidget, QApplication, QTableWidgetItem, QHeaderView, QMessageBox
 from YangTrace import Ui_Form
 from YangTraceCore import get_address
 
@@ -17,18 +17,24 @@ class MainWindow(QWidget, Ui_Form):
 
     def traceroute(self):
         target = self.webInput.text()
+        if len(target) <= 3:
+            self.show_dialog("输入的网址长度过小，请重新输入！")
+            return
         max_ttl = 30
         try:
             target_ip = socket.gethostbyname(target)
         except socket.gaierror:
             print(f"Could not resolve {target}")
+            self.show_dialog("输入的网址错误，找不到对应的IP地址！")
             return
         for ttl in range(1, max_ttl + 1):
             result = get_address(target_ip, ttl)
             print(result)
             self.add_row(result)
             if result[1] == target_ip:
-                break
+                self.show_dialog(f"已到达目标 {target} ，ip：{target_ip}")
+                return
+
 
     def add_row(self, result_list):
         row_position = self.resultOutput.rowCount()
@@ -37,6 +43,16 @@ class MainWindow(QWidget, Ui_Form):
             item = QTableWidgetItem(str(data))
             self.resultOutput.setItem(row_position, col, item)
         QApplication.processEvents()
+
+
+    def show_dialog(self, text):
+        # 创建一个消息盒（弹窗）
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("提示")
+        msg_box.setText(text)
+        msg_box.setIcon(QMessageBox.Information)
+        msg_box.setStandardButtons(QMessageBox.Ok)
+        msg_box.exec()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
